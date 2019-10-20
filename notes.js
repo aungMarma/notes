b9212badb3f6cfa39a08171102424bc4ddffec9f
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2019-10-20 01:25:48 -0400

    refactor notes module

--- notes.js
+++ notes.js
@@ -11,10 +11,10 @@ const removeNote = function(title) {
 		return note.title !== title;
 	});
 	if (notes.length > nonRemovedNotes.length) {
-		saveNotes(nonRemovedNotes, true);
+		saveNotes(nonRemovedNotes);
 		console.log(chalk.green.inverse('Note removed successfully!'));
 	} else {
-		console.log(chalk.yellow.inverse(`There is no note with title ${chalk.blue(title)}!`));
+		console.log(chalk.red.inverse(`No note found!`));
 	}
 };
 
@@ -29,16 +29,18 @@ const addNote = function(title, body) {
 			body
 		});
 		saveNotes(notes);
+		console.log(chalk.green.inverse('New note added!'));
 	} else {
-		console.log(chalk.yellow.inverse(`Note title ${chalk.blue(title)} taken!`));
+		console.log(chalk.red.inverse(`Note title taken!`));
 	}
 };
 
-const saveNotes = function(notes, remove) {
-	const notesJSON = JSON.stringify(notes);
-	fs.writeFileSync('notes.json', notesJSON);
-	if (remove === undefined || remove === false) {
-		console.log(chalk.green.inverse('New note added successfully!'));
+const saveNotes = function(notes) {
+	try {
+		const notesJSON = JSON.stringify(notes);
+		fs.writeFileSync('notes.json', notesJSON);
+	} catch (error) {
+		console.log(chalk.red.inverse("Notes can't be saved now"));
 	}
 };
 
